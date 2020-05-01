ed77f3a17c0d843abd93ae1ba571929119c763d9
clericpy <clericpy@gmail.com>
2020-05-01 21:08:43 +0800

    更新值得买版本号

--- Javascript/tampermonkey/zhidemai.js
+++ Javascript/tampermonkey/zhidemai.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         什么值得买抓取评论
 // @namespace    https://github.com/ClericPy/somethings
-// @version      0.1
+// @version      0.2
 // @description  什么值得买
 // @author       Clericpy
 // @match        http*://search.smzdm.com/*
